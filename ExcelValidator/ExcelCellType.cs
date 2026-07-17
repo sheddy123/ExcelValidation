30b467f13e1ee256adfc93c14ea84724cd2471d6
Ifeanyi Shadrach Odom <ifeanyishadrach452@gmail.com>
2026-07-17 19:07:32 +0100

    Sync changes

--- ExcelValidator/ExcelCellType.cs
+++ ExcelValidator/ExcelCellType.cs
@@ -3,6 +3,7 @@ namespace ExcelValidator
     /// <summary>
     /// The kind of value a column is expected to hold.
     /// </summary>
+    /// <remarks>
     public enum ExcelCellType
     {
         /// <summary>Any value. Every non-empty cell satisfies this type.</summary>
